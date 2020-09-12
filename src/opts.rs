43c5fb865882186e6d36866f9b849d4b55b411a9
Marvin Countryman <me@maar.vin>
2020-09-12 15:01:16 -0400

    Add env var `PORT`

--- src/opts.rs
+++ src/opts.rs
@@ -3,7 +3,7 @@ use clap::Clap;
 
 #[derive(Clap, Clone)]
 pub struct AppOpts {
-  #[clap(short, long, default_value = "8080")]
+  #[clap(short, long, env = "PORT", default_value = "8080")]
   pub port: u16,
 
   #[clap(short, long, env = "REDIS_URL")]
